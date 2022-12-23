ec521a9afaaa0941ad3ceb8b96e8a43815acddc5
Moritz Renkin <moritz@renkin.cc>
2022-12-23 22:41:08 +0100

    added 3_6 and 3_8

--- BI_Projects/BI_62/task3/3_6.sql
+++ BI_Projects/BI_62/task3/3_6.sql
@@ -1,18 +1,28 @@
 SELECT
-    *
-FROM
-    (
-    SELECT
-        ProductTopCategoryName,
-        ProductSubCategoryName,
-        ProductModelName,
-        SUM(OrderQty) AS QuantitySold
-    FROM
-        BI_BikesDW_62.Fact_InternetSales
-    JOIN BI_BikesDW_62.Dim_Product
-        ON Fact_InternetSales.ProductKey = Dim_Product.ProductKey
-    GROUP BY ProductTopCategoryName, ProductSubCategoryName,ProductModelName
-    ORDER BY QuantitySold DESC
+    grouped_ranked.ProductTopCategoryName,
+    grouped_ranked.ProductModelName,
+    grouped_ranked.QuantitySold
+FROM (
+    SELECT grouped.ProductTopCategoryName,
+           grouped.ProductModelName,
+           grouped.QuantitySold,
+           -- MySQL 8.0.0 and above support Window functions like ROW_NUMBER() OVER
+           -- Using Window functions to solve top-n-per-group problems is generally preferable due to performance considerations
+           ROW_NUMBER() OVER (
+               PARTITION BY ProductTopCategoryName
+               ORDER BY QuantitySold DESC
+               ) ranked_order
+    FROM (
+        SELECT
+            ProductTopCategoryName,
+            ProductModelName,
+            SUM(OrderQty) AS QuantitySold
+        FROM BI_BikesDW_62.Fact_InternetSales
+            JOIN BI_BikesDW_62.Dim_Product
+                ON Fact_InternetSales.ProductKey = Dim_Product.ProductKey
+        GROUP BY ProductTopCategoryName, ProductModelName
+         ) AS grouped
+     ) AS grouped_ranked
 
-    ) AS ordered_by_QuantitySold
-
+WHERE grouped_ranked.ranked_order <= 3
+ORDER BY ProductTopCategoryName ASC, QuantitySold DESC;
--- BI_Projects/BI_62/task3/3_8.sql
+++ BI_Projects/BI_62/task3/3_8.sql
@@ -2,8 +2,8 @@ SELECT
   MONTH(OrderDate) AS OrderMonth,
   SUM(OrderLineTaxAmt) AS TotalTaxAmount
 FROM BI_BikesDW_62.Fact_InternetSales InternetSales
-    JOIN BI_BikesDW_62.Dim_Location Location
-    ON InternetSales.ShipToLocationKey = Location.LocationKey
+    JOIN BI_BikesDW_62.Dim_Location ShipLocation
+        ON InternetSales.ShipToLocationKey = ShipLocation.LocationKey
 WHERE
     (Country = 'France' OR Country = 'Germany')
     AND OrderDate BETWEEN '2021-01-01' AND '2021-06-30'
